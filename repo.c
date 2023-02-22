979fd958fed917e0a5254888104750fd7fc14fb7
JohnsonMasino <johnsonmasino@gmail.com>
2023-02-22 15:38:15 -0800

    repo

---
+++ repo.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/*
+ * main - prints the name of this repository.
+ * Return: 0.
+ * Code by Masino.
+ */
+
+int main(void)
+{
+	printf("The name of this GitHub repository is #more_C\n");
+	printf("\nCode developed by Masino\n");
+	return (0);
+}
