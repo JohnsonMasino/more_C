483f443e591d180e6809fe1c06d176ae7c9a11cc
JohnsonMasino <johnsonmasino@gmail.com>
2023-02-22 15:50:22 -0800

    Owner's File

---
+++ owner.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/*
+ * main - prints the details of the owner of this github.
+ * Return: 0.
+ * Code by Masino.
+ */
+
+int main(void)
+{
+	printf("Welcome to this GitHub\n");
+	printf("\nThe owner of this GitHub is:\nName:\tJohnson Masino Obinna\nGitHub:\tJohnsonMasino@github\nE-mail:\tjohnsonmasino@gmail.com\n");
+	return (0);
+}
