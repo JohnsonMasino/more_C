2862b6a973d008318678ace73e157033ce8dd49c
JohnsonMasino <johnsonmasino@gmail.com>
2023-03-02 03:27:06 -0800

    Electricity issue

---
+++ now.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/*
+ * My code for the day
+ */
+
+int main(void)
+{
+	printf("I am having issues of electricity in my location\nMy gagdets are all down so there is no codes for today\n");
+	printf("Masino's github account here\n");
+	return (0);
+}
