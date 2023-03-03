dba48d29525379344a1abfc40cc75baa8e601266
JohnsonMasino <johnsonmasino@gmail.com>
2023-03-03 15:58:09 -0800

    My light just got fixed!

---
+++ good.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/*
+ * main - prints a message.
+ * Code by Masino.
+ */
+
+int main(void)
+{
+	printf("Finally my light got fixed and I can write codes again!\n\nThis is Masino's GitHub account\n");
+	return (0);
+}
