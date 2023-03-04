5d4fc01207162a4476ada3a7df582c19fbb9ead7
JohnsonMasino <johnsonmasino@gmail.com>
2023-03-04 13:26:36 -0800

    Switch and Enum

--- switchenum.c
+++ switchenum.c
@@ -8,21 +8,47 @@
 
 int main(void)
 {
-	int a, b;
-	
-	printf("Enter two numbers\n");
-	scanf("%d %d", &a, &b);
-	switch (a + 1)
+	int a;
+
+	enum wkdys
 	{
+		mon = 0,
+		teu = 1,
+		wed = 2,
+		thur = 3,
+		fri = 4,
+		sat = 5,
+		sun = 6};
+	
+	printf("Enter a number here:\n");
+	scanf("%d", &a);
+	switch (a)
+	{
+		case 0:
+			printf("Today is Monday\n");
+			break;
+		case 1:
+			printf("Today is Teusday\n");
+			break;
 		case 2:
-			printf("Sum is:\n%d\n", a + b);
+			printf("Today is Wednesday\n");
 			break;
 		case 3:
-			printf("Diff is:\n%d\n", a - b);
+			printf("Today is Thursday\n");
 			break;
-		default:
-			printf("Thanks for listening\n");
+		case 4:
+			printf("Today is Friday\n");
+			break;
+		case 5:
+			printf("Today is Saturday\n");
+			break;
+		case 6:
+			printf("Today is Sunday\n");
+			break;
+		default :
+			printf("These are the weekdays\n");
 			break;
 	}
+	printf("\nCode developed by Masino.\n");
 	return (0);
 }
