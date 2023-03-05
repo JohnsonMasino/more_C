b141a9514295014960b2b0804fa51a06ee931ced
JohnsonMasino <johnsonmasino@gmail.com>
2023-03-05 01:14:24 -0800

    extern from global declration

---
+++ extern.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+/*
+ * main - prints content from an extended variable.
+ * Return: 0.
+ * Code by Masino.
+ */
+
+void fun1();
+void fun2();
+
+void main(void)
+{
+	extern int x;
+
+	printf("In main block, X from fun1 is: %d\n", x);
+	fun1();
+	fun2();
+	printf("\nCode devloped by Masino\n");
+}
+
+void fun1(void)
+{
+	int x = 1;
+
+	x += 5;
+	printf("X in fun1 is: %d\n", x);
+}
+
+void fun2(void)
+{
+	int y = 4;
+
+	y++;
+	printf("In fun2, y = %d\n", y);
+}
+
+int x = 10;
