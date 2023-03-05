457a10dbd9827b21cca0d14f28a002b4ae52e710
JohnsonMasino <johnsonmasino@gmail.com>
2023-03-05 15:59:19 -0800

    Changed File Question

--- gate.c
+++ gate.c
@@ -1,23 +1,30 @@
 #include <stdio.h>
 
+/*
+ * main - calls a function and performs some actions.
+ * Return: 0.
+ * Code by Masino.
+ */
+
 void disp();
 
 int a, b, c = 0;
 
 int main(void)
 {
-	static int a = 1;
+	auto int a = 1;
 
 	disp();
 	a += 1;
 	disp();
 	printf("From main block:\nA = %d\nB = %d\n", a, b);
+	printf("\nCode developed by MAsino\n");
 	return (0);
 }
 
 void disp(void)
 {
-	static int a = 2;
+	register int a = 2;
 	int b = 1;
 	a += ++b;
 	printf("From function block:\nA = %d\nB = %d\n", a, b);
