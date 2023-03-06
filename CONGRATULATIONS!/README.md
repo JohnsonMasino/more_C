73bc704d3da1b26f4d617e2e31f76999f5f6aa57
JohnsonMasino <johnsonmasino@gmail.com>
2023-03-05 18:03:03 -0800

    Through with C Program

---
+++ CONGRATULATIONS!/README.md
@@ -0,0 +1,6 @@
+                  CONGRATULATIONS!!!
+0. I have just completed my C Programming Language Course!!!
+1. I am so excited and so happy about my New Development.
+2. Now I can't wait to explore Python Programming Language.
+3. Henceforth, I will be writing codes in Python!
+4. Thanks to alx Africa for giving me direction >>>
---
+++ CONGRATULATIONS!/congrats.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/*
+ * main - prints a congratulatory message.
+ * Return: (NULL).
+ * Code by Masino.
+ */
+
+void main(void)
+{
+	printf("Congratulations to Masino for completing his course on C-Programming_Language\n");
+	printf("Completed on Monday, 6th March, 2023.\n");
+	printf("Hello Python! Here I come!!!\nMessage developed by Masino\n");
+	printf("Thank you alx Africa for directions and motivation\n");
+	printf("Wondering what the date and date are...?\nI got you!\n");
+	printf("The current date is: %s(MM:DD:YY)\n", __DATE__);
+	printf("The current time is: %s(HH:MM:SS)\n", __TIME__);
+}
