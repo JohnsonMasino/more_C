b74bf9d2119876b86b4430d0b432f77c0c6fe420
JohnsonMasino <johnsonmasino@gmail.com>
2023-08-05 05:19:16 -0700

    Remembering

---
+++ practice/file1.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+/*
+ * main: prints a random defined function.
+ * Return 0 always
+ * Code developed by Masino.
+ */
+
+int main(void)
+{
+	int a = 10, b = 12;
+	int *p = &a;
+
+	printf("%d\n", a);
+	printf("%d\n", *p);
+	return (0);
+}
